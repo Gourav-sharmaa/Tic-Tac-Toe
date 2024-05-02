6fafc584e18dc86ce19b46fa8cb896310bf82167
unknown <gourav-gourav2@live.nmit.ac.nz>
2024-05-02 19:10:46 +0530

    tic tac toe

--- Main.js
+++ Main.js
@@ -4,4 +4,22 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 }
-);
+)
+function initializeGame() {
+  return Array(3).fill().map(() => Array(3).fill(null))
+
+}
+
+function getPlayerInput(player, grid) {
+  rl.question(`player ${player}, enter your move(row,col): `, (input) => {
+    const [row, col] = input.split(',').map(Number)
+    if ( grid[rew][col] === null){
+      grid [row][col] = player
+      callback(grid)
+    } else {
+      console.log ("spot taken, try again")
+      getPlayerInput(player, grid)
+    }
+  
+  })
+}
