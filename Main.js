1260f1c7e2b2955563c1fd2fe8db105beb032a10
unknown <gourav-gourav2@live.nmit.ac.nz>
2024-05-02 19:27:09 +0530

    tic tac toe

--- Main.js
+++ Main.js
@@ -23,3 +23,11 @@ function getPlayerInput(player, grid) {
   
   })
 }
+
+function checkWin(grid, player) {
+  const conditions =[
+    [grid[o][0], grid[0][1], grid[0][2]],
+    [grid[1][0], grid[1][1], grid[1][2]],
+    [grid[2][0], grid[2][1], grid[2][2]],
+  ]
+}
